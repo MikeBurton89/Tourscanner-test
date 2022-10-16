824b0a139a962ae19edf02a7902320a5232f5a33
Michele <michele.puricelli@sintegra.it>
2022-10-16 10:56:46 +0200

    added modal

--- src/AllImagesContainer.jsx
+++ src/AllImagesContainer.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
-import { Box, Grid, Typography, IconButton, Stack, Tooltip } from '@mui/material'
+import { Box, Grid, Typography, IconButton, Stack, } from '@mui/material'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { TabPanel } from './TabSystem'
+import TitleBox from './TitleBox'
 import { getNumberOfSaves } from './services/getNumberOfSaves'
+import SaveImageModal from './SaveImageModal'
 
 import { styled } from '@mui/material/styles'
+import { handleBreakpoints } from '@mui/system';
 
 const StyledImage = styled(Box)(() =>
 ({
+    padding: 0,
     margin: `10px 5px`,
-    height: '300px',
-    width: `270px`,
+    height: '270px',
+    width: `300px`,
     objectFit: 'cover'
 }))
 
@@ -19,6 +23,9 @@ function AllImagesContainer() {
     const [images, setImages] = useState([])
     const [error, setError] = useState(null)
     const [isLoading, setIsLoading] = useState(true)
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     useEffect(() => {
 
@@ -46,20 +53,22 @@ function AllImagesContainer() {
             <Grid item xs={12}>
                 <Grid container>
                     {images && images.map((image) => <>
-                        <Grid item xs={12} sm={6} md={4} lg={3} xl={2} justifyItems='flex-start'>
+                        <Grid item xs={10} sm={6} md={6} lg={4} xl={2} justifyItems='flex-start'>
                             <StyledImage
                                 component='img'
                                 loading='lazy'
                                 src={image.url}
                                 key={image.image_id}
-                                onClick={() => getNumberOfSaves(image.image_id)} />
-                            <Stack direction='row' justifyContent='space-between'>
-                                <IconButton><ArrowDropDownIcon /></IconButton>
-                                <Typography sx={{ width: '60%', margin: '10px 5px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellpisis' }} variant='caption'>{image.title}</Typography>
-                            </Stack>
+                                onClick={() => {
+                                    getNumberOfSaves(image.image_id)
+                                    handleOpen()
+
+                                }} />
+                            <TitleBox title={image.title} />
                         </Grid>
                     </>)}
                 </Grid>
+                <SaveImageModal open={open} handleClose={handleClose} ></SaveImageModal>
             </Grid>
         </TabPanel >
     )
---
+++ src/TitleBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Box, Grid, Typography, IconButton, Stack, } from '@mui/material'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+
+
+function TitleBox(props) {
+    return (
+        <Stack direction='row' justifyContent='space-between'>
+            <IconButton><ArrowDropDownIcon /></IconButton>
+            <div style={{
+                display: `webkit-box`,
+                WebkitLineClamp: 3,
+                WebkitBoxOrient: 'vertical',
+                width: '100%',
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellpisis',
+                margin: '10px 5px',
+            }}>
+                <Typography variant='caption'>{props.title}</Typography>
+            </div>
+        </Stack>
+    )
+}
+
+export default TitleBox
